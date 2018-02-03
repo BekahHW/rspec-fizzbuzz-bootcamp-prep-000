6f7fc27f82a8a8732defdc65989024ac1060928d
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-02-03 21:55:37 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if int % 3 == 0
+  if (int % 3 == 0)
   puts  "Fizz",
   if int % 5 == 0
     puts "Buzz"
     if int % 5 && 3 == 0
-      puts fizzbuzz
+      puts FizzBuzz
 end
 end
