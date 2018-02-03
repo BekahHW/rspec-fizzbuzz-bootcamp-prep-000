7078a58bab750f291d9c9e926a88c52debbf6e4b
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-02-03 21:58:05 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
   if (int % 3 == 0)
   puts  "Fizz",
-  if int % 5 == 0
+  elsif (int % 5 == 0)
     puts "Buzz"
-    if int % 5 && 3 == 0
+    elsif (int % 5) && (3 == 0)
       puts FizzBuzz
 end
 end
